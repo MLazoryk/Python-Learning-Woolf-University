325257b39676d1afdaf66f89e1df564b7f3a0a70
Maksym <maksym.lazoryk@gmail.com>
2025-12-10 17:48:45 +0000

    Woolf

--- Material2.py
+++ Material2.py
@@ -1,6 +1,20 @@
-name = "Maksym"
-age = 18
-gpa = 3.2
+# TASK1
+# name = "Maksym"
+# age = 18
+# gpa = 3.2
+# is_student = True
+# print(type(is_student) )
+
+
+# TASK2
+name = "Maksym" # str
+age = 18 # int
+gpa = 3.2 # float
 is_student = True
 
-print(type(name))
+gpa = int(gpa)
+print(gpa)
+
+age = float(age)
+print(age)
+
