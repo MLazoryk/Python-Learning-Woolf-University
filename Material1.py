4c88f649ccfb062425b802fd31c5ee20c13689b7
Maksym <maksym.lazoryk@gmail.com>
2025-12-10 16:47:58 +0000

    woolf

--- Material1.py
+++ Material1.py
@@ -1,1 +1,23 @@
-print("Hello\nWorld")
+#TASK 1
+#print("Hello\nWorld")
+
+
+
+#TASK 2
+#text = "apple, banana, cherry"
+#result = text.split(',')
+#print(result)
+
+
+
+#TASK 3
+#elements = ['earth', 'air', 'fire', 'water']
+#result = ', '.join(elements)
+#print(result)
+
+
+
+#TASK 4
+#text = "Hello world"
+#new_text = text.replace("world", "Python")
+#print(new_text)
