ba42e7492a41084900d062ac32ff9b2cfb7b7420
Maksym <maksym.lazoryk@gmail.com>
2025-12-11 20:23:23 +0000

    WOOLF

---
+++ TOPIC6.py
@@ -0,0 +1,11 @@
+# Define a function named 'say_hello' that takes one parameter called 'name' 
+def say_hello(name):
+
+# Print a greeting message that includes the 'name' parameter
+    print(f'Hello, {name}')
+
+# Print a message indicating that this file (hello.py) has been imported
+print("You imported hello.py")
+
+# Call the 'say_hello' function with the string 'user' as the argument 
+say_hello('user')
